7521460f8b3fc8d739c68f807c2a02d25679efcd
Óscar de Arriba <odarriba@gmail.com>
2015-10-11 17:13:53 +0200

    Add some packages to use middlewares for views and logging.

--- initializers/application.js
+++ initializers/application.js
@@ -3,10 +3,20 @@
 module.exports = function(app) {
   var expressSession = require('express-session'),
       bodyParser  = require("body-parser"),
+      morgan = require("morgan"),
       methodOverride = require("method-override"),
+      compression = require('compression'),
       config = app.get('config');
 
   app.use(expressSession({secret: config.app.secret, resave: false, saveUninitialized: false}));
+
+  app.set('views', config.root + '/server/views');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
+  
+  app.use(compression());
+  app.use(morgan('dev'));
+
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
